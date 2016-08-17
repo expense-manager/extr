939bb8ce88497ebe4726e388ff744d588f591626
Zekun Wang <zwang172@gmail.com>
2016-08-16 20:10:01 -0500

    Add basic category and category detail

--- app/src/main/java/com/expensemanager/app/models/Category.java
+++ app/src/main/java/com/expensemanager/app/models/Category.java
@@ -1,12 +1,13 @@
 package com.expensemanager.app.models;
 
+import android.support.annotation.Nullable;
+
+import io.realm.Realm;
 import io.realm.RealmModel;
+import io.realm.RealmResults;
+import io.realm.Sort;
 import io.realm.annotations.PrimaryKey;
 import io.realm.annotations.RealmClass;
-
-/**
- * Created by Zhaolong Zhong on 8/16/16.
- */
 
 @RealmClass
 public class Category implements RealmModel {
@@ -35,5 +36,30 @@ public class Category implements RealmModel {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    // Query
+
+    /**
+     * @return all categories
+     */
+    public static RealmResults<Category> getAllCategories() {
+        Realm realm = Realm.getDefaultInstance();
+        RealmResults<Category> categories = realm.where(Category.class).findAll();
+        realm.close();
+
+        return categories;
+    }
+
+    /**
+     * @param id
+     * @return Category object if exist, otherwise return null.
+     */
+    public static @Nullable Category getCategoryById(String id) {
+        Realm realm = Realm.getDefaultInstance();
+        Category category = realm.where(Category.class).equalTo(ID_KEY, id).findFirst();
+        realm.close();
+
+        return category;
     }
 }
