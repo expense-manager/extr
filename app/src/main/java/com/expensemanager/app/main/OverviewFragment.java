409e9cba6719ec3ca00dafb13bdb302fc5c5fe57
Zhaolong Zhong <zztg2@mail.missouri.edu>
2016-09-06 22:51:17 -0500

    Add progress bar in overview

--- app/src/main/java/com/expensemanager/app/main/OverviewFragment.java
+++ app/src/main/java/com/expensemanager/app/main/OverviewFragment.java
@@ -1,15 +1,14 @@
 package com.expensemanager.app.main;
 
 import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,10 +24,8 @@ import com.expensemanager.app.models.Member;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -42,25 +39,31 @@ import io.realm.RealmResults;
 public class OverviewFragment extends Fragment {
     private static final String TAG = OverviewFragment.class.getSimpleName();
 
-    private int progress = 80;
-    private int status = 0;
     private Handler handler = new Handler();
-
+    private int totalStatus = 0;
+    private int weeklyStatus = 0;
+    private int monthlyStatus = 0;
     private ArrayList<Expense> expenses;
     private OverviewAdapter overviewAdapter;
     private String groupId;
+    private double totalExpense = 0.0;
+    private double weeklyExpense = 0.0;
+    private double weeklyAve = 0.0;
+    private double monthlyExpense = 0.0;
+    private double monthlyAve = 0.0;
 
     @BindView(R.id.overview_fragment_total_text_view_id) TextView totalTextView;
     @BindView(R.id.overview_fragment_weekly_total_text_view_id) TextView weeklyTextView;
     @BindView(R.id.overview_fragment_monthly_total_text_view_id) TextView monthlyTextView;
-    @BindView(R.id.overview_fragment_monthly_label_text_view_id) TextView monthlyLabelTextView;
     @BindView(R.id.overview_fragment_weekly_average_text_view_id) TextView weeklyAverageTextView;
     @BindView(R.id.overview_fragment_monthly_average_text_view_id) TextView monthlyAverageTextView;
     @BindView(R.id.overview_fragment_recycler_view_id) RecyclerView recyclerView;
     @BindView(R.id.overview_fragment_fab_id) FloatingActionButton fab;
-    @BindView(R.id.circular_progressbar) ProgressBar circularProgress;
-    @BindView(R.id.circular_progress_text) TextView progressTextView;
-
+    @BindView(R.id.weekly_circular_progress_text) TextView weeklyProgressTextView;
+    @BindView(R.id.monthly_circular_progress_text) TextView monthlyProgressTextView;
+    @BindView(R.id.overview_total_progressBar) ProgressBar totalProgressBar;
+    @BindView(R.id.overview_weekly_progressBar) ProgressBar weeklyProgressBar;
+    @BindView(R.id.overview_monthly_progressBar) ProgressBar monthlyProgressBar;
 
     public static OverviewFragment newInstance() {
         return new OverviewFragment();
@@ -91,33 +94,110 @@ public class OverviewFragment extends Fragment {
         });
 
         invalidateViews();
-        setupProgress();
     }
 
-    private void setupProgress() {
-        Drawable circularDrawable = ContextCompat.getDrawable(getActivity(), R.drawable.circular);
-        circularProgress.setProgress(0);
-        circularProgress.setSecondaryProgress(100);
-        circularProgress.setMax(100);
-        circularProgress.setProgressDrawable(circularDrawable);
+
+    public void invalidateViews() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(getString(R.string.shared_preferences_session_key), 0);
+        groupId = sharedPreferences.getString(Group.ID_KEY, null);
+
+        totalExpense = getTotalExpense();
+        weeklyExpense = getWeeklyExpense();
+        weeklyAve = getWeeklyAverage();
+        monthlyExpense = getMonthlyExpense();
+        monthlyAve = getMonthlyAverage();
+
+//        totalTextView.setText("$" + new DecimalFormat("##").format(getTotalExpense()));
+        weeklyTextView.setText("$" + new DecimalFormat("##.##").format(weeklyExpense));
+        monthlyTextView.setText("$" + new DecimalFormat("##.##").format(monthlyExpense));
+        weeklyAverageTextView.setText("$" + new DecimalFormat("##.##").format(weeklyAve));
+        monthlyAverageTextView.setText("$" + new DecimalFormat("##.##").format(monthlyAve));
+
+        overviewAdapter.clear();
+        if (Member.getAllAcceptedMembersByGroupId(groupId).size() > 1) {
+            overviewAdapter.setShowMember(true);
+        } else {
+            overviewAdapter.setShowMember(false);
+        }
+        overviewAdapter.addAll(Expense.getAllExpensesByGroupId(groupId));
+
+        Log.d(TAG, "zhaox: weeklyExpense: " + weeklyExpense);
+        Log.d(TAG, "zhaox: weeklyAve: " + weeklyAve);
+
+        int totalProgress = 100;
+        int weeklyProgress = (int)(weeklyExpense/weeklyAve * 100);
+        int monthlyProgress = (int)(monthlyExpense/monthlyAve * 100);
+
+        setupTotalProgress(totalExpense);
+        setupWeeklyProgress(weeklyProgress);
+        setupMonthlyProgress(monthlyProgress);
+    }
+
+    private void setupTotalProgress(final double totalProgress) {
+        totalProgressBar.setMax((int)totalProgress);
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while (status < progress) {
-                    status += 1;
+                try {
+                    Thread.sleep(1600);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
+                int step = 1;
+                if (totalProgress >= 100) {
+                    step = (int) (totalProgress/100);
+                }
+
+                final int finalStep = step;
+                while (totalStatus < totalProgress) {
+                    totalStatus += step;
                     handler.post(new Runnable() {
-
                         @Override
                         public void run() {
-                            circularProgress.setProgress(status);
-                            progressTextView.setText(status + "%");
+                            totalProgressBar.setProgress(totalStatus);
 
+                            if (totalStatus + finalStep >= totalProgress) {
+                                totalTextView.setText("$" + totalProgress);
+                            } else {
+                                totalTextView.setText("$" + totalStatus);
+                            }
                         }
                     });
                     try {
-                        Thread.sleep(20);
+                        Thread.sleep(30);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void setupWeeklyProgress(int weeklyProgress) {
+
+        Log.d(TAG, "zhaox: weeklyProcess: " + weeklyProgress);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1600);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                while (weeklyStatus < weeklyProgress) {
+                    weeklyStatus += 1;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            weeklyProgressBar.setProgress(weeklyStatus);
+                            weeklyProgressTextView.setText(weeklyStatus + "%");
+                        }
+                    });
+                    try {
+                        Thread.sleep(30);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -137,26 +217,63 @@ public class OverviewFragment extends Fragment {
 //        }
     }
 
-    public void invalidateViews() {
-        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(getString(R.string.shared_preferences_session_key), 0);
-        groupId = sharedPreferences.getString(Group.ID_KEY, null);
+    private void setupMonthlyProgress(int monthlyProgress) {
 
-        Calendar calendar = Calendar.getInstance();
+        Log.d(TAG, "zhaox: monthlyProgress: " + monthlyProgress);
 
-        monthlyLabelTextView.setText(calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US));
-        totalTextView.setText("$" + new DecimalFormat("##").format(getTotalExpense()));
-        weeklyTextView.setText("$" + new DecimalFormat("##.##").format(getWeeklyExpense()));
-        monthlyTextView.setText("$" + new DecimalFormat("##.##").format(getMonthlyExpense()));
-        weeklyAverageTextView.setText("$" + new DecimalFormat("##.##").format(getWeeklyAverage()));
-        monthlyAverageTextView.setText("$" + new DecimalFormat("##.##").format(getMonthlyAverage()));
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1600);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
-        overviewAdapter.clear();
-        if (Member.getAllAcceptedMembersByGroupId(groupId).size() > 1) {
-            overviewAdapter.setShowMember(true);
-        } else {
-            overviewAdapter.setShowMember(false);
+                while (monthlyStatus < monthlyProgress) {
+                    monthlyStatus += 1;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            monthlyProgressBar.setProgress(monthlyStatus);
+                            monthlyProgressTextView.setText(monthlyStatus + "%");
+                        }
+                    });
+                    try {
+                        Thread.sleep(30);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+//        if(android.os.Build.VERSION.SDK_INT >= 11){
+//            // will update the "progress" propriety of seekbar until it reaches progress
+//
+//            ObjectAnimator animation2 = ObjectAnimator.ofInt(circularProgress, "progress", 80);
+//            animation2.setDuration(1500); // 0.5 second
+//            animation2.setInterpolator(new DecelerateInterpolator());
+//            animation2.start();
+//        } else {
+//            seekBar.setProgress(80);
+//        }
+    }
+
+    private int getWeeklyProcess() {
+        if (weeklyAve > 1) {
+            return (int)(weeklyExpense / weeklyAve);
         }
-        overviewAdapter.addAll(Expense.getAllExpensesByGroupId(groupId));
+
+        return 0;
+    }
+
+    private int getMonthlyProcess() {
+//        if (getMonthlyAverage() > 1) {
+//            return (int)(getMonthlyExpense() / getMonthlyAverage());
+//        }
+
+        return 0;
     }
 
     private void setupRecyclerView() {
